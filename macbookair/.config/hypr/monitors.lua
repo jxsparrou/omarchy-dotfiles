61e5ef5c7ca63e1eaa718e45020c01deb1ecd594
jxsparrou <jxsparrou@gmail.com>
2026-09-16 18:40:47 -0400

    add macbook monitors.lua file

---
+++ macbookair/.config/hypr/monitors.lua
@@ -0,0 +1,31 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- List current monitors and supported resolutions with: hyprctl monitors all
+
+-- Monitor wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- Example: output can be found with hyprctl monitors. Edit variables.lua for the monitor outputs instead of here directly
+-- hl.monitor({
+--     output    = "MONITOR1",
+--     mode      = "1920x1080@60",
+--     position  = "0x0",
+--     scale     = "1",
+-- })
+
+local omarchy_gdk_scale = 1
+hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
+
+-- local MONITOR1 = "DP-1"
+-- local MONITOR2 = "DP-2"
+
+hl.monitor({
+    output    = "eDP-1",
+    mode      = "preferred",
+    position  = "0x0",
+    scale = 1.7
+})
+
+-- hl.monitor({
+--  	output = MONITOR2,
+--  	mode = "2560x1440@144.00Hz",
+--  	position = "2560x0",
+--  	scale = "1",
+-- })
