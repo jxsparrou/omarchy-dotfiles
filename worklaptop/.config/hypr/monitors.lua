781b9c3a8fe971c6b986c24674f400b425c95553
jxsparrou <jxsparrou@gmail.com>
2026-09-17 12:58:55 -0400

    add work computer and verified it is working. I better be happy about this in the future. I wiped my ~/ in the process

---
+++ worklaptop/.config/hypr/monitors.lua
@@ -0,0 +1,39 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- List current monitors and supported resolutions with: hyprctl monitors all
+
+-- Monitor wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- Example: output can be found with hyprctl monitors. Edit variables.lua for the monitor outputs instead of here directly
+-- hl.monitor({
+--     output    = "MONITOR1",
+--     mode      = "1920x1080@60",
+--     position  = "0x0",
+--     scale     = "1",
+-- })
+
+local omarchy_gdk_scale = 1
+hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
+
+local SCREEN = "eDP-1"
+local LEFT_MON = "DP-3"
+local RIGHT_MON = "HDMI-A-1"
+
+hl.monitor({
+    output    = LEFT_MON,
+    mode      = "1920x1080@74.97Hz",
+    position  = "0x0",
+    scale     = "1",
+})
+
+hl.monitor({
+	output = RIGHT_MON,
+	mode = "1920x1080@74.97Hz",
+	position = "1920x0",
+	scale = "1",
+})
+
+hl.monitor({
+	output = SCREEN,
+	mode = "1920x1200@60.00Hz",
+	position = "1280x1080",
+	scale = "1.5",
+})
---
+++ worklaptop/.config/hypr/workspaces.lua
@@ -0,0 +1,26 @@
+local LEFT_MON = "DP-3"
+local RIGHT_MON = "HDMI-A-1"
+local SCREEN = "eDP-1"
+
+hl.workspace_rule({
+	workspace = "1",
+	monitor = LEFT_MON,
+	persistent = true,
+})
+
+hl.workspace_rule({
+	workspace = "2",
+	monitor = RIGHT_MON,
+	persistent = true,
+})
+
+hl.workspace_rule({
+	workspace = "3",
+	monitor = SCREEN,
+	persistent = true,
+})
+
+hl.workspace_rule({
+	workspace = "5",
+	monitor = "DP-1"
+})
